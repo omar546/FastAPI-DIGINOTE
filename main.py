92e078bbae842547de5a06fccbddafa4a8327b08
Omar Abo El-Khair <71936776+omar546@users.noreply.github.com>
2024-04-28 20:38:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,23 +9,23 @@ app = FastAPI()
 # model = load_model('model.h5')
 
 @app.post("/test/")
-async def upload_image(str: text):
+async def test(text):
     text2 = text + "test back form api"
     return JSONResponse(content={"text": text})
 
-@app.post("/upload/")
-async def upload_image(str: UploadFile):
-    try:
-        # Save the uploaded image to a temporary file
-        # with tempfile.NamedTemporaryFile(delete=True, suffix=".jpg") as temp_image:
-        #     temp_image.write(file.file.read())
-        #     temp_image.seek(0)
+# @app.post("/upload/")
+# async def upload_image(str: UploadFile):
+#     try:
+#         # Save the uploaded image to a temporary file
+#         # with tempfile.NamedTemporaryFile(delete=True, suffix=".jpg") as temp_image:
+#         #     temp_image.write(file.file.read())
+#         #     temp_image.seek(0)
 
-            # Process the image and get the text
-            text = UploadFile + "back from API"
+#             # Process the image and get the text
+#             text = UploadFile + "back from API"
 
-            # Return the text as a response
-            return JSONResponse(content={"text": text})
+# #             # Return the text as a response
+# #             return JSONResponse(content={"text": text})
 
-    except Exception as e:
-        return JSONResponse(content={"error": str(e)}, status_code=500)
+#     except Exception as e:
+#         return JSONResponse(content={"error": str(e)}, status_code=500)
