0b2d47f9b24c2c8198d6df1575a87113df3f70a0
omar546 <omer.mkawy.om@gmail.com>
2024-04-28 22:29:39 +0300

    ps kda

--- main.py
+++ main.py
@@ -3,49 +3,23 @@ from fastapi.responses import JSONResponse
 import tempfile
 from PIL import Image
 from tensorflow.keras.models import load_model
+import numpy as np
 
 import cv2
-from mltu.tensorflow.dataProvider import DataProvider
-from mltu.preprocessors import ImageReader
-from mltu.transformers import ImageResizer, LabelIndexer, LabelPadding, ImageShowCV2
-from mltu.augmentors import RandomBrightness, RandomRotate, RandomErodeDilate, RandomSharpen
-from mltu.annotations.images import CVImage
+
 
 app = FastAPI()
 
 model = load_model('model.h5')
 
 
-from mltu.configs import BaseModelConfigs
+def preprocess_image(image, width, height):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, (width, height))
+    # Further preprocessing if needed
+    return image
 
 
-class ModelConfigs(BaseModelConfigs):
-    def __init__(self):
-        super().__init__()
-        self.model_path = None
-        self.vocab = ""
-        self.height = 32
-        self.width = 128
-        self.max_text_length = 0
-        self.batch_size = 16
-        self.learning_rate = 0.0005
-        self.train_epochs = 20
-        self.train_workers = 20
-
-configs = ModelConfigs()
-
-
-data_provider = DataProvider(
-    dataset=None,
-    skip_validation=True,
-    batch_size=configs.batch_size,
-    data_preprocessors=[ImageReader(CVImage)],
-    transformers=[
-        ImageResizer(configs.width, configs.height, keep_aspect_ratio=False),
-        LabelIndexer(configs.vocab),
-        LabelPadding(max_word_length=configs.max_text_length, padding_value=len(configs.vocab))
-        ],
-)
 
 @app.get("/test/")
 async def test():
@@ -54,16 +28,11 @@ async def test():
 @app.post("/upload/")
 async def upload_image(file: UploadFile):
     try:
-        with tempfile.NamedTemporaryFile(delete=True, suffix=".jpg") as temp_image:
-            temp_image.write(file.file.read())
-            temp_image.seek(0)
-            image = cv2.imread(temp_image)
-
-            preprocessed_image = image.copy()  # Make a copy to avoid modifying the original image
-            for preprocessor in data_provider.data_preprocessors:
-                preprocessed_image = preprocessor(preprocessed_image)
-        
-
+            contents = await file.read()
+            nparr = np.frombuffer(contents, np.uint8)
+            img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+            image = preprocess_image(img,128,32)
+            model.predict(np.expand_dims(image, axis=0))
 
              # Return the text as a response
             return JSONResponse(content={"text": "this is the response"})
